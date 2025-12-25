de62b260f365d994cdb6e6a383c77eaba29293f5
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-25 23:48:27 +0530

    commited by n8n

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,18 +1,22 @@
 from flask import Blueprint, request, jsonify
 from backend.services.user_service import UserService
 from backend.repositories.user_repository import UserRepository
+from backend.services.session_service import SessionService
+from backend.repositories.session_repository import SessionRepository
 
 user_bp = Blueprint('user', __name__)
 user_service = UserService(UserRepository())
+session_service = SessionService(SessionRepository())
 
-@user_bp.route('/register', methods=['POST'])
-def register_user():
+@user_bp.route('/login', methods=['POST'])
+def login_user():
     data = request.get_json()
     try:
-        user = user_service.register_user(
+        user = user_service.authenticate_user(
             email=data['email'],
             password=data['password']
         )
-        return jsonify(user), 201
+        session = session_service.create_session(user['id'])
+        return jsonify({"token": session['token']}), 200
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
