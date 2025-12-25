ce61370bcfecc3dd0b6d773f0707ef60057f0d37
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 00:10:52 +0530

    commited by n8n

--- backend/tests/test_search_service.py
+++ backend/tests/test_search_service.py
@@ -6,41 +6,25 @@ class SearchServiceTestCase(unittest.TestCase):
     def setUp(self):
         self.product_repository = ProductRepository()
         self.search_service = SearchService(self.product_repository)
-        self.product_repository.add_product({
-            'id': 1,
-            'name': 'Product1',
-            'price': 100.0,
-            'description': 'Description1'
-        })
-        self.product_repository.add_product({
-            'id': 2,
-            'name': 'Product2',
-            'price': 200.0,
-            'description': 'Description2'
-        })
 
-    def test_search_products_name(self):
-        results = self.search_service.search_products(query='Product1', page=1)
-        self.assertEqual(len(results['results']), 1)
-        self.assertEqual(results['results'][0]['name'], 'Product1')
+        self.product_repository.add_product({'id': 1, 'name': 'Test Product 1', 'price': 100.0, 'description': 'Description 1'})
+        self.product_repository.add_product({'id': 2, 'name': 'Another Product', 'price': 150.0, 'description': 'Description 2'})
+        self.product_repository.add_product({'id': 3, 'name': 'Test Product 3', 'price': 200.0, 'description': 'Description 3', 'category': 'Electronics'})
+        
+    def test_search_products_by_name(self):
+        results = self.search_service.search_products('Test')
+        self.assertEqual(len(results), 2)
 
-    def test_search_products_description(self):
-        results = self.search_service.search_products(query='Description2', page=1)
-        self.assertEqual(len(results['results']), 1)
-        self.assertEqual(results['results'][0]['description'], 'Description2')
+    def test_search_products_by_description(self):
+        results = self.search_service.search_products('Description 2')
+        self.assertEqual(len(results), 1)
 
-    def test_search_products_no_results(self):
-        results = self.search_service.search_products(query='Nonexistent', page=1)
-        self.assertEqual(len(results['results']), 0)
+    def test_search_products_by_category(self):
+        results = self.search_service.search_products('Electronics')
+        self.assertEqual(len(results), 1)
 
     def test_search_products_pagination(self):
-        for i in range(3, 23):
-            self.product_repository.add_product({
-                'id': i,
-                'name': f'Product{i}',
-                'price': 100.0 * i,
-                'description': f'Description{i}'
-            })
-        results = self.search_service.search_products(query='Product', page=2)
-        self.assertEqual(len(results['results']), 10)
-        self.assertEqual(results['page'], 2)
+        results = self.search_service.search_products('Product', page=1, page_size=2)
+        self.assertEqual(len(results), 2)
+        results = self.search_service.search_products('Product', page=2, page_size=2)
+        self.assertEqual(len(results), 1)
