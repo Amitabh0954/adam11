bb57b9a1cad7d318d2de685caf9d885a0a9744b0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 00:10:53 +0530

    commited by n8n

--- backend/sql/02_create_products_table.sql
+++ backend/sql/02_create_products_table.sql
@@ -2,5 +2,7 @@ CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     price FLOAT NOT NULL CHECK (price > 0),
-    description TEXT NOT NULL
+    description TEXT NOT NULL,
+    category VARCHAR(255),
+    attributes JSONB
 );
