482d916abe7cdad1c9fd72b302c125a8ebc3b984
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-25 23:49:23 +0530

    commited by n8n

---
+++ backend/repositories/product_repository.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from backend.models.product import Product
+
+class ProductRepository:
+    def __init__(self):
+        self.products = {}
+        self.name_index = set()
+
+    def add_product(self, product: Product) -> None:
+        if product['name'] in self.name_index:
+            raise ValueError("Product name must be unique")
+        if product['price'] <= 0:
+            raise ValueError("Product price must be a positive number")
+        if not product['description']:
+            raise ValueError("Product description cannot be empty")
+
+        self.products[product['id']] = product
+        self.name_index.add(product['name'])
+
+    def get_product_by_id(self, product_id: int) -> Optional[Product]:
+        return self.products.get(product_id)
+
+    def get_product_by_name(self, name: str) -> Optional[Product]:
+        for product in self.products.values():
+            if product['name'] == name:
+                return product
+        return None
