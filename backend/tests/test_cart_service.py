1080c67c851e01dc9798e05195de29d96eb80035
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 00:11:53 +0530

    commited by n8n

--- backend/tests/test_cart_service.py
+++ backend/tests/test_cart_service.py
@@ -1,77 +1,37 @@
 import unittest
 from backend.repositories.shopping_cart_repository import ShoppingCartRepository
 from backend.services.carts.cart_service import CartService
-from backend.models.user import User
-from backend.models.product import Product
-from backend.models.shopping_cart import ShoppingCartItem
 
 class CartServiceTestCase(unittest.TestCase):
     def setUp(self):
         self.cart_repository = ShoppingCartRepository()
         self.cart_service = CartService(self.cart_repository)
-        self.user = User(id=1, username='testuser', email='test@example.com')
-        self.product = Product(id=1, name='Product1', price=100.0, description='Description1')
+        self.user_id = 1
 
-    def test_add_product_to_cart(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        cart = self.cart_service.view_cart(self.user)
+    def test_get_or_create_cart(self):
+        cart = self.cart_service.get_or_create_cart(self.user_id)
+        self.assertIsNotNone(cart)
+        self.assertEqual(cart['user_id'], self.user_id)
+
+    def test_add_item_to_cart(self):
+        cart = self.cart_service.add_item_to_cart(self.user_id, product_id=1, quantity=2)
         self.assertEqual(len(cart['items']), 1)
+        self.assertEqual(cart['items'][0]['product_id'], 1)
+        self.assertEqual(cart['items'][0]['quantity'], 2)
 
-    def test_view_cart(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        cart = self.cart_service.view_cart(self.user)
-        self.assertEqual(cart['user']['username'], 'testuser')
-
-    def test_checkout(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        self.cart_service.checkout(self.user)
-        cart = self.cart_service.view_cart(self.user)
+    def test_remove_item_from_cart(self):
+        self.cart_service.add_item_to_cart(self.user_id, product_id=1, quantity=2)
+        cart = self.cart_service.remove_item_from_cart(self.user_id, product_id=1)
         self.assertEqual(len(cart['items']), 0)
 
-    def test_remove_product_from_cart(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        self.cart_service.remove_product_from_cart(self.user, self.product['id'], confirm=True)
-        cart = self.cart_service.view_cart(self.user)
+    def test_clear_cart(self):
+        self.cart_service.add_item_to_cart(self.user_id, product_id=1, quantity=2)
+        self.cart_service.clear_cart(self.user_id)
+        cart = self.cart_service.get_or_create_cart(self.user_id)
         self.assertEqual(len(cart['items']), 0)
-        self.assertEqual(cart['total_price'], 0.0)
 
-    def test_remove_product_without_confirmation(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        with self.assertRaises(ValueError):
-            self.cart_service.remove_product_from_cart(self.user, self.product['id'], confirm=False)
-
-    def test_update_quantity(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        self.cart_service.update_quantity(self.user, self.product['id'], quantity=5)
-        cart = self.cart_service.view_cart(self.user)
-        self.assertEqual(cart['items'][0]['quantity'], 5)
-        self.assertEqual(cart['total_price'], 500.0)
-
-    def test_update_quantity_invalid(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        with self.assertRaises(ValueError):
-            self.cart_service.update_quantity(self.user, self.product['id'], quantity=-1)
-
-    def test_save_cart(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        self.cart_service.save_cart(self.user)
-        # Assuming `load_cart_from_db` returns the correct stored cart data
-        self.cart_service.load_cart(self.user)
-        cart = self.cart_service.view_cart(self.user)
-        self.assertEqual(len(cart['items']), 1)
-
-    def test_load_cart(self):
-        cart_item = ShoppingCartItem(product=self.product, quantity=1)
-        self.cart_service.add_product_to_cart(self.user, cart_item)
-        self.cart_service.save_cart(self.user)
-        self.cart_service.load_cart(self.user)
-        cart = self.cart_service.view_cart(self.user)
-        self.assertEqual(len(cart['items']), 1)
+    def test_delete_cart(self):
+        cart = self.cart_service.get_or_create_cart(self.user_id)
+        self.cart_service.delete_cart(self.user_id)
+        cart = self.cart_service.get_or_create_cart(self.user_id)
+        self.assertEqual(len(cart['items']), 0)
