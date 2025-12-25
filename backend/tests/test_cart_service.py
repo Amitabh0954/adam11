bd4bef9519f8b596af5b3ff79ce7af5abf24467a
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-25 23:51:52 +0530

    commited by n8n

---
+++ backend/tests/test_cart_service.py
@@ -0,0 +1,32 @@
+import unittest
+from backend.repositories.shopping_cart_repository import ShoppingCartRepository
+from backend.services.carts.cart_service import CartService
+from backend.models.user import User
+from backend.models.product import Product
+from backend.models.shopping_cart import ShoppingCartItem
+
+class CartServiceTestCase(unittest.TestCase):
+    def setUp(self):
+        self.cart_repository = ShoppingCartRepository()
+        self.cart_service = CartService(self.cart_repository)
+        self.user = User(id=1, username='testuser', email='test@example.com')
+        self.product = Product(id=1, name='Product1', price=100.0, description='Description1')
+
+    def test_add_product_to_cart(self):
+        cart_item = ShoppingCartItem(product=self.product, quantity=1)
+        self.cart_service.add_product_to_cart(self.user, cart_item)
+        cart = self.cart_service.view_cart(self.user)
+        self.assertEqual(len(cart['items']), 1)
+
+    def test_view_cart(self):
+        cart_item = ShoppingCartItem(product=self.product, quantity=1)
+        self.cart_service.add_product_to_cart(self.user, cart_item)
+        cart = self.cart_service.view_cart(self.user)
+        self.assertEqual(cart['user']['username'], 'testuser')
+
+    def test_checkout(self):
+        cart_item = ShoppingCartItem(product=self.product, quantity=1)
+        self.cart_service.add_product_to_cart(self.user, cart_item)
+        self.cart_service.checkout(self.user)
+        cart = self.cart_service.view_cart(self.user)
+        self.assertEqual(len(cart['items']), 0)
