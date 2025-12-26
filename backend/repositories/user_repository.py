d219431075bea29752b89e4cf1428783be8983ee
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:03:46 +0530

    commited by n8n

--- backend/repositories/user_repository.py
+++ backend/repositories/user_repository.py
@@ -1,22 +1,11 @@
-from typing import Optional
-from backend.models.user import User
+from models.user import User
 
 class UserRepository:
     def __init__(self):
-        self.users = {}
-        self.email_index = set()
-
-    def add_user(self, user: User) -> None:
-        if user['email'] in self.email_index:
-            raise ValueError("Email must be unique")
-        self.users[user['id']] = user
-        self.email_index.add(user['email'])
-
-    def get_user_by_id(self, user_id: int) -> Optional[User]:
-        return self.users.get(user_id)
-
-    def get_user_by_email(self, email: str) -> Optional[User]:
-        for user in self.users.values():
-            if user['email'] == email:
-                return user
-        return None
+        self.users = []
+    
+    def find_by_email(self, email: str) -> User:
+        return next((user for user in self.users if user.email == email), None)
+    
+    def save(self, user: User) -> None:
+        self.users.append(user)
