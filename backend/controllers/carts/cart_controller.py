37cc9a3df1056aa5dfc927c18ce307aa2df9d1d9
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:52:44 +0530

    commited by n8n

--- backend/controllers/carts/cart_controller.py
+++ backend/controllers/carts/cart_controller.py
@@ -7,23 +7,26 @@ cart_service = CartService()
 @cart_controller.route('/carts', methods=['POST'])
 def add_product_to_cart():
     data = request.get_json()
-    response = cart_service.add_product_to_cart(data)
+    user_id = request.headers.get('User-ID')
+    response = cart_service.add_product_to_cart(data, user_id)
     return jsonify(response), response['status']
 
 @cart_controller.route('/carts/<int:item_id>', methods=['DELETE'])
 def remove_product_from_cart(item_id: int):
     confirm = request.args.get('confirm', False, type=bool)
-    response = cart_service.remove_product_from_cart(item_id, confirm)
+    user_id = request.headers.get('User-ID')
+    response = cart_service.remove_product_from_cart(item_id, confirm, user_id)
     return jsonify(response), response['status']
 
 @cart_controller.route('/carts', methods=['GET'])
 def get_cart_items():
-    user_id = request.args.get('user_id')
+    user_id = request.headers.get('User-ID')
     response = cart_service.get_cart_items(user_id)
     return jsonify(response), response['status']
 
 @cart_controller.route('/carts/<int:item_id>', methods=['PUT'])
 def update_cart_item_quantity(item_id: int):
     data = request.get_json()
-    response = cart_service.update_cart_item_quantity(item_id, data)
+    user_id = request.headers.get('User-ID')
+    response = cart_service.update_cart_item_quantity(item_id, data, user_id)
     return jsonify(response), response['status']
