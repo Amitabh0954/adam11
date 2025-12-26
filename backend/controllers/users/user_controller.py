4e99a4cb617df4d427cd8b0a699ae2c0914a3a2a
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:03:44 +0530

    commited by n8n

---
+++ backend/controllers/users/user_controller.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, request, jsonify
+from services.users.user_service import UserService
+
+user_controller = Blueprint('user_controller', __name__)
+user_service = UserService()
+
+@user_controller.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    response = user_service.register_user(data)
+    return jsonify(response), response['status']
