e8f74370aaa231f0cc535c66eff0555a39353d62
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:03:59 +0530

    commited by n8n

--- backend/configurations/config.py
+++ backend/configurations/config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAX_LOGIN_ATTEMPTS = int(os.environ.get('MAX_LOGIN_ATTEMPTS', 5))
+    SESSION_TIMEOUT = int(os.environ.get('SESSION_TIMEOUT', 1800))  # 30 minutes
