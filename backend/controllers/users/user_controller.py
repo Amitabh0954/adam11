5be3d213974f2e3bc8a1866711fe038f93d33e09
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:04:20 +0530

    commited by n8n

--- backend/controllers/users/user_controller.py
+++ backend/controllers/users/user_controller.py
@@ -15,3 +15,15 @@ def login():
     data = request.get_json()
     response = user_service.login_user(data)
     return jsonify(response), response['status']
+
+@user_controller.route('/password-reset', methods=['POST'])
+def request_password_reset():
+    data = request.get_json()
+    response = user_service.request_password_reset(data)
+    return jsonify(response), response['status']
+
+@user_controller.route('/password-reset/<token>', methods=['POST'])
+def reset_password(token: str):
+    data = request.get_json()
+    response = user_service.reset_password(data, token)
+    return jsonify(response), response['status']
