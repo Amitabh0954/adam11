3f05598564dc117f7541e9d7be51882878bb7278
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:04:42 +0530

    commited by n8n

--- backend/controllers/users/user_controller.py
+++ backend/controllers/users/user_controller.py
@@ -27,3 +27,9 @@ def reset_password(token: str):
     data = request.get_json()
     response = user_service.reset_password(data, token)
     return jsonify(response), response['status']
+
+@user_controller.route('/profile', methods=['PUT'])
+def update_profile():
+    data = request.get_json()
+    response = user_service.update_profile(data)
+    return jsonify(response), response['status']
