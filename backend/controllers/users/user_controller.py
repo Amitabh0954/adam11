8cda150ef302a7f9a215d5a3129803b64bf11a49
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:46:57 +0530

    commited by n8n

---
+++ backend/controllers/users/user_controller.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, request, jsonify
+from services.users.user_service import UserService
+
+user_controller = Blueprint('user_controller', __name__)
+user_service = UserService()
+
+@user_controller.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    response = user_service.register_user(data)
+    return jsonify(response), response['status']
+
+@user_controller.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    response = user_service.login_user(data)
+    return jsonify(response), response['status']
+
+@user_controller.route('/password-reset', methods=['POST'])
+def request_password_reset():
+    data = request.get_json()
+    response = user_service.request_password_reset(data)
+    return jsonify(response), response['status']
+
+@user_controller.route('/password-reset/<token>', methods=['POST'])
+def reset_password(token: str):
+    data = request.get_json()
+    response = user_service.reset_password(data, token)
+    return jsonify(response), response['status']
+
+@user_controller.route('/profile', methods=['PUT'])
+def update_profile():
+    data = request.get_json()
+    response = user_service.update_profile(data)
+    return jsonify(response), response['status']
