36bd1a352c41c6a0d2b8a75410f22b75805c95a3
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:11:21 +0530

    commited by n8n

--- backend/repositories/product_repository.py
+++ backend/repositories/product_repository.py
@@ -7,20 +7,4 @@ class ProductRepository:
     def find_by_name(self, name: str) -> Product:
         return next((product for product in self.products if product.name == name), None)
     
-    def find_by_id(self, product_id: int) -> Product:
-        return next((product for product in self.products if product.id == product_id), None)
-    
-    def save(self, product: Product) -> None:
-        self.products.append(product)
-    
-    def update(self, product: Product) -> None:
-        index = next((i for i, p in enumerate(self.products) if p.id == product.id), None)
-        if index is not None:
-            self.products[index] = product
-    
-    def delete(self, product: Product) -> None:
-        self.products.remove(product)
-    
-    def search(self, query: str) -> list[Product]:
-        query = query.lower()
-        return [product for product in self.products if query in product.name.lower() or query in product.description.lower()]
+    def find_by_id(self, product_id:
