5b27303fdab54a41a183f4f8f770315b2b09b22c
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:27:34 +0530

    commited by n8n

--- backend/repositories/product_repository.py
+++ backend/repositories/product_repository.py
@@ -1,35 +1,25 @@
-from typing import Optional, List
-from backend.models.product import Product
+from models.product import Product
 
 class ProductRepository:
     def __init__(self):
-        self.products = {}
-        self.name_index = set()
-
-    def add_product(self, product: Product) -> None:
-        if product['name'] in self.name_index:
-            raise ValueError("Product name must be unique")
-        if product['price'] <= 0:
-            raise ValueError("Product price must be a positive number")
-        if not product['description']:
-            raise ValueError("Product description cannot be empty")
-
-        self.products[product['id']] = product
-        self.name_index.add(product['name'])
-
-    def get_product_by_id(self, product_id: int) -> Optional[Product]:
-        return self.products.get(product_id)
-
-    def get_product_by_name(self, name: str) -> Optional[Product]:
-        for product in self.products.values():
-            if product['name'] == name:
-                return product
-        return None
-
-    def get_all_products(self) -> List[Product]:
-        return list(self.products.values())
-
-    def remove_product(self, product_id: int) -> None:
-        product = self.products.pop(product_id, None)
-        if product:
-            self.name_index.remove(product['name'])
+        self.products = []
+    
+    def find_by_name(self, name: str) -> Product:
+        return next((product for product in self.products if product.name == name), None)
+    
+    def find_by_id(self, product_id: int) -> Product:
+        return next((product for product in self.products if product.id == product_id), None)
+    
+    def save(self, product: Product) -> None:
+        self.products.append(product)
+    
+    def update(self, product: Product) -> None:
+        index = next((i for i, p in enumerate(self.products) if p.id == product.id), None)
+        if index is not None:
+            self.products[index] = product
+    
+    def delete(self, product: Product) -> None:
+        self.products = [p for p in self.products if p.id != product.id]
+    
+    def search(self, query: str) -> list[Product]:
+        return [product for product in self.products if query.lower() in product.name.lower()]
