1d5e1c990bd7e28b0b99703cf4dc84f39800a09b
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:03:43 +0530

    commited by n8n

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from configurations.config import Config
+from controllers.users import user_controller
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    with app.app_context():
+        app.register_blueprint(user_controller)
+    
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000)
