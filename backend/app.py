7aa349576c057f240b3619e34c6f2a649bd27599
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-26 09:25:28 +0530

    commited by n8n

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from configurations.config import Config
+from controllers.users import user_controller
+from controllers.products import product_controller
+from controllers.carts import cart_controller
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    with app.app_context():
+        app.register_blueprint(user_controller)
+        app.register_blueprint(product_controller)
+        app.register_blueprint(cart_controller)
+    
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000)
